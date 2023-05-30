539bca45ca907eef6469b17ff99c8480569e5d43
daisuke <daisuke.zr@icloud.com>
2023-05-31 00:33:27 +0900

    bs version added

--- common.py
+++ common.py
@@ -6,66 +6,47 @@ from oauth2client.service_account import ServiceAccountCredentials
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 import bs4
+import traceback
+import os
+from dotenv import load_dotenv
 from time import sleep 
-class InsertEnglishMeaning:
-    
-    def __init__(self, url):
-        self.url = url
+
+
+class Common():
+
+    def __init__(self, url=None):
+        if url is not None:
+            self.url = url
+        else:
+            load_dotenv(os.path.dirname(os.path.abspath(__file__)))
+            self.url = os.environ.get("SPREADSHEET_URL")
+            print(self.url)
         self.connect_to_spreadsheet()
-        self.connect_to_webdriver()
         self.rows = []
+
 
 
     def connect_to_spreadsheet(self):
         scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-        path = r'C:\Users\daisu\Periodical\google-service-account.json'
+        path = r'C:\workspace\envs\english_vocab\google-service-account.json'
         credentials = ServiceAccountCredentials.from_json_keyfile_name(path, scope)
         gc = gspread.authorize(credentials)
         key = self.url.split("/")[5]
         self.workbook = gc.open_by_key(key)
+        self.ws =self.workbook.worksheets()[-1]
 
 
-    def connect_to_webdriver(self):
-        self.driver = webdriver.Chrome(ChromeDriverManager().install())
-
+    def already_checked(self, row):
+        cells = row
+        return cells[3].value != ""
+    
     def get_row(self, ws, count):
         columns_num = 5
         first_row_index = 8
         first_column_index = 2
         current_index = first_row_index + count
         return ws.range(current_index, first_column_index, current_index, columns_num)
-
-    def already_checked(self, row):
-        cells = row
-        return cells[3].value != ""
-
-    def search_meaning(self, row):
-        cells = row
-        name = cells[0].value
-        url = f"https://dictionary.cambridge.org/dictionary/english/{name}"
-        self.driver.get(url)
-        soup = bs4.BeautifulSoup(self.driver.page_source, 'html.parser')
-        name = soup.select('.di-title')[0].text if len(soup.select('.di-title')) > 0 else "Not Found"
-        row_definitions = soup.select('.def')
-        row_examples = soup.select('.examp')
-        current_url = self.driver.current_url
-        examples = []
-        definitions = []
-        for index, row_example in enumerate(row_examples):
-            example = row_example.text
-            examples.append(example)
-            if index == 5:
-                break
-
-        for index, row_definition in enumerate(row_definitions):
-            definition = row_definition.text
-            definition = definition.replace(':', "")
-            definitions.append(definition)
-
-
-
-        return name, definitions, examples, current_url
-
+    
     def insert_info(self, ws, row, info):
         cells = row
         cells[0].value = info["name"]
@@ -92,13 +73,11 @@ class InsertEnglishMeaning:
     def ws_already_checked(self, ws):
         value = ws.acell('A3').value
         return value == "TRUE"
-
-    def test(self):
-        self.already_checked(self.workbook.worksheets()[0])
-        
-        
-
-    def __call__(self):
+    
+    def search_meaning(self, row):
+        raise Exception("you should override this method")
+    
+    def main(self):
         try:
             for ws in self.workbook.worksheets():
                 count = -1
@@ -109,21 +88,14 @@ class InsertEnglishMeaning:
                     row = self.get_row(ws, count)
                     if self.already_checked(row) == False:
                         if self.has_word(row):
-                            name, definitions, examples, url = self.search_meaning(row)
-                            info = {"name": name, "definitions": definitions, "examples": examples, "url":url}
-                            if definitions != []:
+                            info = self.search_meaning(row)
+                            if info["definitions"] != []:
                                 self.insert_info(ws, row, info)
                         else:
                             print("you have checked every word!!")
                             checked_every_word = True
+        except:
+            print(traceback.format_exc())
         finally:
-            self.driver.close()
-
-    
-
-if __name__ == "__main__":
-    spreadsheet_url = "https://docs.google.com/spreadsheets/d/1QtdcbvmGHlgA-3E2lSHj9CNLhGKmQDBhvO5AoVSivtU/edit#gid=1577131084"
-    instance = InsertEnglishMeaning(spreadsheet_url)
-    instance()
-                  
-        
+            input("終了するにはEnterを押してください。")
+            
---
+++ insert_english_meaning_with_bs.py
@@ -0,0 +1,35 @@
+
+import requests
+from bs4 import BeautifulSoup as bs4
+
+from common import Common
+
+
+class EnglishMeaningInserter(Common):
+
+    def search_meaning(self, row):
+        cells = row
+        name = cells[0].value
+        url = f"https://www.merriam-webster.com/dictionary/{name}"
+        print(url)
+        res = requests.get(url)
+        soup = bs4(res.text, 'html.parser')
+        row_definitions = soup.find_all("a", class_="fb share-link")
+        row_examples = soup.find_all("span", class_="d-block t has-aq")
+        examples = []
+        definitions = []
+        for index, row_example in enumerate(row_examples):
+            example = row_example.text
+            examples.append(example)
+            if index == 5:
+                break
+
+        for index, row_definition in enumerate(row_definitions):
+            _definitions = row_definition.attrs['data-share-description'].replace("… See the full definition", "").split(";")
+            definitions.extend(_definitions)
+        info = {"name": name, "definitions": definitions, "examples": examples, "url":url}
+        return info
+
+if __name__ == "__main__":
+    inserter = EnglishMeaningInserter()
+    inserter.main()
